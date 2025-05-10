476269d42e3cb7e392c5d412ec4a37f2194d1bb0
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-05-10 23:20:15 +0400

    Reapply "added project"
    This reverts commit df68772a6a087b82429ceeeb33fa7b6e61d3a17d.

---
+++ src/menu.py
@@ -0,0 +1,22 @@
+menu = [
+    {
+        "id": 1,
+        "title": "Salad",
+        "img": "salad.jpeg",
+        "ingredients": ["Tomato", "Salad", "Cucumber"],
+        "comments": [
+            {"text": "I loved it!", "rating": 8.0},
+            {"text": "Too salty for me", "rating": 5.0}
+        ]
+    },
+    {
+        "id": 2,
+        "title": "Burger",
+        "img": "burger.jpg",
+        "ingredients": ["Beef", "Cheese", "Buns"],
+        "comments": [
+            {"text": "Amazing", "rating": 7.9},
+            {"text": "It's okay", "rating": 6.1}
+        ]
+    }
+]
